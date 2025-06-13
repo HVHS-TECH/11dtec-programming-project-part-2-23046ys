25f0314331e74b6685b3ec4bb0d7602992cd2425
23046ys <23046ys@hvhs.school.nz>
2025-06-13 02:31:32 +0000

    Jun 13, 2025, 2:31 AM

--- index.js
+++ index.js
@@ -9,10 +9,15 @@ The purpose of this is to make the index page more interactive
 
 //Variables
 var budget
+var size
+var furniture
 var recommended
 var choice
 let properties = ["","","","","",""]
 let weeklyPrices = [450.00, 455.70, 479.98, 590.58, 620.00]
+let availableFurniture = []
+let furniturePrices = [15.57, 18.99, 25.33, 44.90, 66.00]
+
 var timeFor
 /****************************
 Main Code:
