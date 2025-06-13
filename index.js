61db4149f0efefcee04006238a787bb6f0067715
23046ys <23046ys@hvhs.school.nz>
2025-06-13 03:11:41 +0000

    Jun 13, 2025, 3:11 AM

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
  Index
   Yuv Singh
   29/04/2025
-The purpose of this is to make the index page more interactive
+The purpose of this is to make the website more interactive and functional
  ***************************/
 
 
