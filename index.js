cbba7a6db58ed194a6ca37018226651776fcf280
23046ys <23046ys@hvhs.school.nz>
2025-06-13 02:24:24 +0000

    Jun 13, 2025, 2:24 AM

--- index.js
+++ index.js
@@ -11,8 +11,8 @@ The purpose of this is to make the index page more interactive
 var budget
 var recommended
 var choice
-
-
+let properties = ["","","","","",""]
+let prices = [50.00, 55.70, 79.98, 90.58, 120.00]
 /****************************
 Main Code:
  ***************************/
