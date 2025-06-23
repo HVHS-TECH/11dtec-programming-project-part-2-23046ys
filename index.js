c9c22e50a394b07c5a2a5eb1c740e23fcae43a13
23046ys <23046ys@hvhs.school.nz>
2025-06-23 21:02:16 +0000

    Jun 23, 2025, 9:02 PM

--- index.js
+++ index.js
@@ -11,13 +11,10 @@ The purpose of this is to make the website more interactive and functional
     var budget
     var size
     var purpose
-    var furniture
     var recommended
     var choice
       let properties = ["Cabin","Small House","Small Vintage House","Medium Modern House","Theater","Lakeside Cabin"];
       let weeklyPrices = [450.00, 455.70, 479.98, 590.58, 620.00, 783.30];
-      let availableFurniture = [];
-      let furniturePrices = [15.57, 18.99, 25.33, 44.90, 66.00];
     var timeFrame
     var timeFor
     var usageFee
