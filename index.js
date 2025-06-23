a6fb90f2ca64972d1fbd20df4a762127fc69b740
23046ys <23046ys@hvhs.school.nz>
2025-06-23 21:42:52 +0000

    Jun 23, 2025, 9:42 PM

--- index.js
+++ index.js
@@ -16,7 +16,6 @@ The purpose of this is to make the website more interactive and functional
       let weeklyArray = [450.00, 455.70, 479.98, 590.58, 620.00, 783.30];
     var timeFrame
     var timeFor
-    var usageFee
     var age
     var confirm
 
