0a7ad2e4e2caa19d7b6b4224aeb9df9da7d8ab1c
23046ys <23046ys@hvhs.school.nz>
2025-06-26 01:13:19 +0000

    Jun 26, 2025, 1:13 AM

--- index.js
+++ index.js
@@ -142,5 +142,17 @@ function calculateRecommendation() {
 }
 
 function confirmationOfThePurchaseToAnnoyPeople(){
-  
+  if(){
+    
+  }
+  if(age < 18){
+    alert("You are too young to rent a property.")
+  }else{
+    confirm= prompt("Type your selected property into the prompt to confirm your purchase.");
+    if(confirm == propertiesArray[choice]){
+      purchased= "Not not"
+    }else{
+      alert("Purchase cancelled.")
+    }
+  }
 }
